9ef171aad7ad55bcdd987356a7cba4f34634753a
raziyekavurgaci <raziyekavurgaci1234@gmail.com>
2024-10-08 12:09:51 +0300

    update

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
-let count = 0;
-const counterElement = document.getElementById("decreaseButton");
-const increaseButton = document.getElementById("increaseButton");
-const counter = document.getElementById("counter");
-
-increaseButton.addEventListener("click", function () {
-  count++;
-  counter.textContent = count;
-});
-decreaseButton.addEventListener("click", function () {
-  count--;
-  counter.textContent = count;
-});
-let audio = document.getElementById("piano");
-
-function music_event() {
-  return audio.paused ? audio.play() : audio.pause();
+function handleCount() {
+  let count = document.getElementById("count");
+  let countNumber = Number(count.innerHTML);
+  countNumber++;
+  count.innerHTML = countNumber;
+  if (count.innerHTML === "5") {
+    alert("Biraz ara vermeslisin.");
+  }
 }
+const sominePlayAudio = document.getElementById("sominePlayAudio");
+const sominePlay = document.getElementById("sominePlay");
+
+sominePlay.addEventListener("click", function () {
+  if (sominePlayAudio.paused) {
+    sominePlayAudio.play();
+    sominePlay.className = "bi bi-boombox-fill";
+  } else {
+    sominePlayAudio.pause();
+    sominePlay.className = "bi bi-music-player-fill";
+  }
+});
+
+const pianoPlayAudio = document.getElementById("pianoPlayAudio");
+const pianoPlay = document.getElementById("pianoPlay");
+
+pianoPlay.addEventListener("click", function () {
+  if (pianoPlayAudio.paused) {
+    pianoPlayAudio.play();
+    pianoPlay.className = "bi bi-boombox-fill";
+  } else {
+    pianoPlayAudio.pause();
+    pianoPlay.className = "bi bi-music-player-fill";
+  }
+});
+
+const rainPlayAudio = document.getElementById("rainPlayAudio");
+const rainPlay = document.getElementById("rainPlay");
+
+rainPlay.addEventListener("click", function () {
+  if (rainPlayAudio.paused) {
+    rainPlayAudio.play();
+    rainPlay.className = "bi bi-boombox-fill";
+  } else {
+    rainPlayAudio.pause();
+    rainPlay.className = "bi bi-music-player-fill";
+  }
+});
+
+const volumeRange = document.getElementById("volume-input");
+volumeRange.addEventListener("input", function () {
+  sominePlayAudio.volume = volumeRange.value / 100;
+  pianoPlayAudio.volume = volumeRange.value / 100;
+  rainPlayAudio.volume = volumeRange.value / 100;
+});
